82bab30b05a43a9f2835ec13d294977e4fa38772
nikolai07-92 <53357795+nikolai07-92@users.noreply.github.com>
2019-07-27 06:11:25 +0500

    Update ImageProcessing.cpp

--- HR_cpp/ImageProcessing.cpp
+++ HR_cpp/ImageProcessing.cpp
@@ -14,8 +14,8 @@ Constructor
 ImageProcessing::ImageProcessing(int cores)
 {
 	if (cores) {
-        _cores = cores;
-    }
+        	_cores = cores;
+    	}
 	else {
 		_cores = sysconf(_SC_NPROCESSORS_ONLN); // get number of cores
 	}
@@ -33,8 +33,8 @@ Convert an image from color t grayscale
 void ImageProcessing::cvtToGray(cv::Mat image_src)
 {
 	int channels    = image_src.channels();
-	int width	    = image_src.cols;
-	int height	    = image_src.rows;
+	int width	= image_src.cols;
+	int height	= image_src.rows;
 	uchar b, g, r, avg;
 	uchar* f_ptr	= image_src.data;
 
@@ -45,8 +45,8 @@ void ImageProcessing::cvtToGray(cv::Mat image_src)
 			r = f_ptr[channels * (rowIdx*width + colIdx) + 2];
 			avg = (b + g + r) / channels;
 			for (size_t ch = 0; ch < channels; ch++) {
-                f_ptr[channels * (rowIdx * width + colIdx) + ch] = avg;
-            }
+                		f_ptr[channels * (rowIdx * width + colIdx) + ch] = avg;
+            		}
 		}
 	}
 }
@@ -59,9 +59,9 @@ Kernel ImageProcessing::getGaussian(int kernel_radius)
 {
 	Kernel kernel(2 * kernel_radius + 1, std::vector<double>(2 * kernel_radius + 1)); // kernel matrix
 
-	double sigma 	= kernel_radius / 2.;
+	double sigma	= kernel_radius / 2.;
 	double mean 	= kernel.size() / 2;
-	double sum 	    = 0;
+	double sum 	= 0;
 
 	//Calculate value
 	for (int row = 0; row < kernel.size(); row++) {
@@ -70,13 +70,13 @@ Kernel ImageProcessing::getGaussian(int kernel_radius)
 				/ (2 * M_PI * sigma * sigma);
 			sum += kernel[row][col];
 		}
-    }
+    	}
 	// Normalization
 	for (int row = 0; row < kernel.size(); row++) {
 		for (int col = 0; col < kernel[row].size(); col++) {
 			kernel[row][col] /= sum;
-        }
-    }
+        	}
+    	}
 	return kernel;
 }
 /*
@@ -96,14 +96,14 @@ cv::Mat ImageProcessing::GaussianBlur(cv::Mat image)
 	cvtToGray(image);
 
 	int row_start	= 0;
-	int	row_end		= 0;
+	int row_end	= 0;
 	//Distribution of parts of image on the threads and start of the gauss blur function
 	for (unsigned int i = 0; i < _cores; i++) {
 		row_start = row_end;
 		row_end += floor(image.rows / _cores);
 		if ((image.rows % _cores) > i) {
-            row_end++;
-        }
+            		row_end++;
+        	}
 		v_threads[i] = std::thread(&ImageProcessing::threadFunction, this, row_start, row_end);
 	}
 	// Waitin for execution of threads
@@ -138,11 +138,11 @@ Image blur function
 void ImageProcessing::applyFilter(cv::Mat image_src, cv::Mat image_dst, Kernel filter, int row_start, int row_end)
 {
 	int i, j, h, w, src_h, src_w;
-	int height		    = image_src.rows;
-	int width		    = image_src.cols;
+	int height		= image_src.rows;
+	int width		= image_src.cols;
 	int filterHeight	= filter.size();
 	int filterWidth		= filter[0].size();
-	int radius		    = (filterHeight - 1) / 2;
+	int radius		= (filterHeight - 1) / 2;
 
 	uchar* dst_ptr = image_dst.data; // ptr to image data
 	uchar* src_ptr = image_src.data; // ptr to image data
@@ -159,8 +159,8 @@ void ImageProcessing::applyFilter(cv::Mat image_src, cv::Mat image_dst, Kernel f
 					}
 					// for border processing
 					if (src_w >= width) {
-                        src_w -= (src_w - width) * 2 + 1;
-                    }
+                        			src_w -= (src_w - width) * 2 + 1;
+                   			}
 					double filter_val = filter[h - i + radius][w - j + radius];
 					double src_val = src_ptr[image_src.channels() * (abs(src_h) * width + abs(src_w))];
 					dst_ptr[i * width + j] += filter_val * src_val;
